e3917306ed2b388059ed68258433fe0e05c1629a
Federico Acosta <fede.acosta2396@gmail.com>
2023-10-11 21:02:28 -0300

    agregada pagina temporal para mostrar login

--- src/pages/LoggedUserPage.jsx
+++ src/pages/LoggedUserPage.jsx
@@ -8,11 +8,18 @@ export default function LoggedUserPage({ currentUser }) {
   };
   return (
     <div className="container">
-      <div className="container-text">
-        <h1>Bienvenido</h1>
-        <h1> {currentUser.email}</h1>
-        <button onClick={logOut}>Cerrar Sesion</button>
+      <div className="avatar-user">
+        {currentUser?.photoURL ? (
+          <img src={currentUser?.photoURL} />
+        ) : (
+          <div className="avatar-user-default"></div>
+        )}
       </div>
+      <h1 className="msg-bienvenida">Bienvenido</h1>
+      <h1 className="user-email"> {currentUser.email}</h1>
+      <button className="cerrar-sesion" onClick={logOut}>
+        Cerrar Sesion
+      </button>
     </div>
   );
 }
